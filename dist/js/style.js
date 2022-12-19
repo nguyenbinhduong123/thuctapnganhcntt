e59f0b175114b44c5dd648fddb0d83b2814986f9
nguyenbinhduong123 <binhduong800@gmail.com>
2022-12-19 09:56:18 +0700

    update index

---
+++ dist/js/style.js
@@ -0,0 +1,9 @@
+// You can also use "$(window).load(function() {"
+$(function () {
+			
+    // Slideshow 1
+    $("#slider1").responsiveSlides({
+      maxwidth: 1600,
+      speed: 600
+    });
+});
